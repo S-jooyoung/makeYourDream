08db3dfac94fec4ba59ecf05d1a840d9038f0199
S-jooyoung <jooyoung.dev@gmail.com>
2022-03-12 20:00:00 +0000

    Style:modify header && footer styles

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import styles from "./footer.module.css";
 const Footer = (props) => {
   return (
     <footer className={styles.footer}>
-      <h1 className={styles.content}>모든 사람들의 꿈이 이루어질때까지</h1>
+      <h1 className={styles.content}>© 2022 All Rights Reserved, Jooyoung®</h1>
     </footer>
   );
 };
