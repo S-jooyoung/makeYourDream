fabcb8c3c686895439112b3f74976516c118707f
S-jooyoung <jooyoung.dev@gmail.com>
2022-03-20 20:00:00 +0000

    Fix:remove mandart component && mandart_outer

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -39,7 +39,19 @@ const Main = ({ authservice, pptmaker }) => {
             "다양한 신년계획을 제공하는 무료 신년계획만들기 메이크드림(MakeDream}입니다. 하단에 있는 원하시는 계획표를 선택 해 주세요."
           }
         />
-        <Button name={"데모"} onClick={(_ev) => pptmaker.mandart()}></Button>
+        <div className={styles.articleWrap}>
+          <article className={styles.article}>
+            <h1>만다르트 계획표</h1>
+            <p>
+              8가지의 큰 키워드를 잡고, 다시 그 키워드를 실천해가기 위한 8가지의
+              행동계획을 작성하는 것입니다.
+            </p>
+            <Button
+              name={"다운로드"}
+              onClick={(_ev) => pptmaker.mandart()}
+            ></Button>
+          </article>
+        </div>
       </section>
       <Footer />
     </div>
