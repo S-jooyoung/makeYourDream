92831e111f68995d10cdb4979ded6fafe9222551
S-jooyoung <jooyoung.dev@gmail.com>
2022-04-14 20:00:00 +0000

    Feat: 월간계획표,주간계획표,일간계획표 레이아웃 구성 in schedulemaker

--- src/components/schedulemaker/schedulemaker.jsx
+++ src/components/schedulemaker/schedulemaker.jsx
@@ -9,7 +9,7 @@ const ScheduleMaker = ({ pptmaker }) => {
       <Banner
         title={"계획 만들기"}
         description={
-          "다양한 신년계획을 제공하는 무료 신년계획만들기 메이크드림(MakeDream)입니다. 하단에 있는 원하시는 계획표를 선택 해 주세요."
+          "다양한 신년 계획을 제공하는 무료 계획 만들기 MakeDream입니다. 하단에 있는 원하시는 계획표를 선택해 주세요."
         }
       />
       <ul className={styles.schedules}>
@@ -18,6 +18,27 @@ const ScheduleMaker = ({ pptmaker }) => {
           title={"만다르트 계획표"}
           info={
             "8가지의 큰 키워드를 잡고, 다시 그 키워드를 실천해가기 위한 8가지의 행동계획을 작성하는 것입니다."
+          }
+        />
+        <Schedule
+          pptmaker={pptmaker}
+          title={"월간 계획표"}
+          info={
+            "월 단위로 해야 할 일과 목표를 설정해두고 행동 계획을 작성하는 것입니다."
+          }
+        />
+        <Schedule
+          pptmaker={pptmaker}
+          title={"주간 계획표"}
+          info={
+            "주 단위로 해야 할 일과 목표를 설정해두고 행동 계획을 작성하는 것입니다."
+          }
+        />
+        <Schedule
+          pptmaker={pptmaker}
+          title={"일일 계획표"}
+          info={
+            "하루를 시작하기 전 오늘 해야 할 일에 대한 행동 계획을 작성하는 것입니다."
           }
         />
       </ul>
