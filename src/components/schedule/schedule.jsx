c9c8cd57a4a853f6a7df89cb5560658efb237690
S-jooyoung <jooyoung.dev@gmail.com>
2022-04-15 23:07:31 +0900

    Refactor: schedule컴포넌트 버튼 shedulemaker로 이동

--- src/components/schedule/schedule.jsx
+++ src/components/schedule/schedule.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import Button from "../button/button";
+
 import styles from "./schedule.module.css";
 
-const Schedule = ({ pptmaker, title, info }) => (
-  <li className={styles.schedule}>
+const Schedule = ({ title, info }) => (
+  <>
     <h1 className={styles.title}>{title}</h1>
     <p className={styles.info}>{info}</p>
-    <div className={styles.button}>
-      <Button name={"다운로드"} onClick={(_ev) => pptmaker.mandart()}>
-        만다르트
-      </Button>
-    </div>
-  </li>
+  </>
 );
 
 export default Schedule;
--- src/components/schedulemaker/schedulemaker.jsx
+++ src/components/schedulemaker/schedulemaker.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../banner/banner";
 import Schedule from "../schedule/schedule";
+import Button from "../button/button";
 import styles from "./schedulemaker.module.css";
 
 const ScheduleMaker = ({ pptmaker }) => {
@@ -12,35 +13,59 @@ const ScheduleMaker = ({ pptmaker }) => {
           "다양한 신년 계획을 제공하는 무료 계획 만들기 MakeDream입니다. 하단에 있는 원하시는 계획표를 선택해 주세요."
         }
       />
+
       <ul className={styles.schedules}>
-        <Schedule
-          pptmaker={pptmaker}
-          title={"만다르트 계획표"}
-          info={
-            "8가지의 큰 키워드를 잡고, 다시 그 키워드를 실천해가기 위한 8가지의 행동계획을 작성하는 것입니다."
-          }
-        />
-        <Schedule
-          pptmaker={pptmaker}
-          title={"월간 계획표"}
-          info={
-            "월 단위로 해야 할 일과 목표를 설정해두고 행동 계획을 작성하는 것입니다."
-          }
-        />
-        <Schedule
-          pptmaker={pptmaker}
-          title={"주간 계획표"}
-          info={
-            "주 단위로 해야 할 일과 목표를 설정해두고 행동 계획을 작성하는 것입니다."
-          }
-        />
-        <Schedule
-          pptmaker={pptmaker}
-          title={"일일 계획표"}
-          info={
-            "하루를 시작하기 전 오늘 해야 할 일에 대한 행동 계획을 작성하는 것입니다."
-          }
-        />
+        <li className={styles.schedule}>
+          <Schedule
+            pptmaker={pptmaker}
+            title={"만다르트 계획표"}
+            info={
+              "8가지의 큰 키워드를 잡고, 다시 그 키워드를 실천해가기 위한 8가지의 행동계획을 작성하는 것입니다."
+            }
+          />
+          <div className={styles.button}>
+            <Button
+              name={"다운로드"}
+              onClick={() => pptmaker.mandart()}
+            ></Button>
+          </div>
+        </li>
+        <li className={styles.schedule}>
+          <Schedule
+            pptmaker={pptmaker}
+            title={"월간 계획표"}
+            info={
+              "월 단위로 해야 할 일과 목표를 설정해두고 행동 계획을 작성하는 것입니다."
+            }
+          />
+          <div className={styles.button}>
+            <Button name={"다운로드"}></Button>
+          </div>
+        </li>
+        <li className={styles.schedule}>
+          <Schedule
+            pptmaker={pptmaker}
+            title={"주간 계획표"}
+            info={
+              "주 단위로 해야 할 일과 목표를 설정해두고 행동 계획을 작성하는 것입니다."
+            }
+          />
+          <div className={styles.button}>
+            <Button name={"다운로드"}></Button>
+          </div>
+        </li>
+        <li className={styles.schedule}>
+          <Schedule
+            pptmaker={pptmaker}
+            title={"일일 계획표"}
+            info={
+              "하루를 시작하기 전 오늘 해야 할 일에 대한 행동 계획을 작성하는 것입니다."
+            }
+          />
+          <div className={styles.button}>
+            <Button name={"다운로드"} />
+          </div>
+        </li>
       </ul>
     </section>
   );
